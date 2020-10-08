52f2935167acfa2e4da4a461642808f94ed0fe4b
Bryan Lilburn <bryanlilburn@gmail.com>
2020-10-07 23:45:02 -0700

    Added Javascript skeleton. Updated portfolio.html with project

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("hello");
+
+let firstProject = document.querySelector(".imageOverlay");
+
+firstProject.addEventListener("click", () => {
+    window.open("https://aroseca15.github.io/Disaster-Ready/", "_blank");
+});
