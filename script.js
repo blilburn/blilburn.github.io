8775d8cb2ca9d1ec76e4cde424586b9292401403
Bryan Lilburn <bryanlilburn@gmail.com>
2020-11-11 23:16:33 -0800

    Added project to portfolio

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let firstProject = document.querySelector(".imageOverlay");
 firstProject.addEventListener("click", () => {
     window.open("https://aroseca15.github.io/Disaster-Ready/", "_blank");
 });
+
+let secondProject = document.querySelector(".imageOverlayTwo");
+
+secondProject.addEventListener("click", () => {
+    window.open("https://immense-meadow-08366.herokuapp.com/", "_blank");
+});
